cd5ac80b9b8fe64f48e02ae6e69d4bcf272383ed
gwelle <guillaume.welle.sio@gmail.com>
2020-12-27 12:02:33 +0100

    test sur l 'age du USER

--- app/validator/validateUser.js
+++ app/validator/validateUser.js
@@ -1,14 +1,11 @@
 
-
-
-
    exports.checkEmail=function (email){
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(String(email).toLowerCase());
     }
 
    exports.checkNames= function (name){
-       const regex=/^[a-zA-Zéèêëàù -]$/
+       const regex=/^[a-zA-Z]$/
        return regex.test(name);
     }
 
@@ -18,17 +15,48 @@
     }
 
     exports.checkAge=function (birthdate){
-         
-        var now = new Date().getTime();
-        var birth=new Date(birthdate).getTime()
-        var Difference_In_Time = now - birth; 
-        var Difference_In_Days=Difference_In_Time/ (1000 * 3600 * 24); 
-        if(Difference_In_Days/365 > 13){
-            return true
-        } 
-        else{
-            return false;
+
+        const regex=/^(0[1-9]{1}|1[0-2]{1})-(0[1-9]{1}|1[0-9]{1}|2[0-9]{1}|3[0-1]{1})-19[0-9]{2}|20[0-9]{2}$/;
+        if(regex.test(birthdate)){
+
+            var now = new Date().getTime();
+            var birth=new Date(birthdate).getTime()
+            
+            if(birth > now){
+
+                return "The date of birth is not valid";
+            }
+
+            else{
+
+                var Difference_In_Time = now - birth;
+                var Difference_In_Days=Difference_In_Time/ (1000 * 3600 * 24);
+
+                if(Difference_In_Days/365 > 13){
+                    return true
+                }
+
+                else{
+                    return false;
+                }
+            }
+
+        }
+
+        else {
+
+            return "invalid date birth";
         }
     }
 
-    
+   exports.isValid=function (user){
+
+     return( this.checkEmail(user["email"])
+         && this.checkNames(user["lastName"])
+         && this.checkNames(user["firstName"])
+         && this.checkAge(user["birthDate"])
+        && this.checkPassword(user["password"])
+     ) ? true : false
+   }
+
+
--- test/testUser.js
+++ test/testUser.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 const userValidator=require('../app/validator/validateUser')
 const todoListValidator=require('../app/validator/validateTodoList')
 
-describe("validator checkEmail()", () => {
+/*describe("validator checkEmail()", () => {
 	it("should return false ", ()=> {
 		expect(userValidator.checkEmail('jules')).to.be.false
     })
@@ -22,7 +22,7 @@ describe("validator checkEmail()", () => {
     it("should return false",()=>{
         expect(userValidator.checkEmail('jules31@.com.gmail.fr')).to.be.false
     })
-  });
+  });*/
 
 // describe("Validator checkNames()",()=>{
 //     it("should return true ", ()=> {
@@ -50,7 +50,7 @@ describe("validator checkEmail()", () => {
 // });
 
 
-describe("Validator checkPassword",()=>{
+/*describe("Validator checkPassword",()=>{
 
   it("it should return false", ()=>{
     expect(userValidator.checkPassword('1234@')).to.be.false
@@ -74,6 +74,52 @@ describe("Validator checkPassword",()=>{
     expect(userValidator.checkPassword('abcd31/$')).to.be.false
   })
   
-});
+});*/
+
+describe( "Validator age", () => {
+
+    it("it should return false", ()=>{
+        expect(userValidator.checkAge('11-27-2020')).to.be.false
+    })
+
+    it("it should return true", ()=>{
+        expect(userValidator.checkAge('11-27-2000')).to.be.true
+    })
+
+    it("it should return invalid date birth", ()=>{
+        expect(userValidator.checkAge('11-27-1899')).to.equal("invalid date birth")
+    })
+
+    it("it should return true", ()=>{
+        expect(userValidator.checkAge('11/27/2000')).to.be.true
+    })
+
+    it("it should return The date of birth is not valid", ()=>{
+        expect(userValidator.checkAge('12-28-2020')).to.equal("The date of birth is not valid")
+    })
+
+    it("it should return invalid date birth", ()=>{
+        expect(userValidator.checkAge('franck')).to.equal("invalid date birth")
+    })
+
+})
+
+
+/*describe("validator isValid()", () => {
+
+    it("should return true ", ()=> {
+
+	    let user = {
+
+            "lastName": "Guillaume",
+            "firstName": "WELLE",
+            "birthDate": "01/04/1993",
+            "email": "gwelle@myges.fr",
+            "password": "test123$",
+        }
+		expect(userValidator.isValid(user)).to.be.true
+    })
+})*/
+
 
 
