d4baad8dab7d875f34f1ace3d86ec7fdcf32c3aa
GABIAM <julesakouete31@gmail.com>
2020-12-27 10:09:35 +0100

    init

--- app/validator/validateUser.js
+++ app/validator/validateUser.js
@@ -4,17 +4,16 @@
 
    exports.checkEmail=function (email){
     const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    console.log(regex.test(String(email).toLowerCase()))
     return regex.test(String(email).toLowerCase());
     }
 
    exports.checkNames= function (name){
-       const regex=/^[A-Za-z]+([\s]{0,1}|['-]{0,1})^$/
-       return /^[a-zA-Z ]+$/.test(name);
+       const regex=/^[a-zA-Zéèêëàù -]$/
+       return regex.test(name);
     }
 
    exports.checkPassword=function (password){
-        const regex=/^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,40}$/;
+        const regex=/^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z])(?=.*?[a-z]).{8,40}$/;
         return regex.test(password);
     }
 
--- test/testUser.js
+++ test/testUser.js
@@ -22,45 +22,58 @@ describe("validator checkEmail()", () => {
     it("should return false",()=>{
         expect(userValidator.checkEmail('jules31@.com.gmail.fr')).to.be.false
     })
+  });
+
+// describe("Validator checkNames()",()=>{
+//     it("should return true ", ()=> {
+// 		expect(userValidator.checkNames('al ain')).to.be.true
+//     })
+//     it("should return true ", ()=> {
+// 		expect(userValidator.checkNames('toto')).to.be.true
+//     })
+//     it("should return true ", ()=> {
+// 		expect(userValidator.checkNames('to-to')).to.be.true
+//     })
+//     it("should return false", ()=> {
+//       expect(userValidator.checkNames('jules31')).to.be.false
+//     })
+//     it("should return false ", ()=> {
+//       expect(userValidator.checkNames('-jules')).to.be.false
+//     })
+//     it("should return false ", ()=> {
+//       expect(userValidator.checkNames('guillaume@')).to.be.false
+//     })
+//     it("should return true ", ()=> {
+//       expect(userValidator.checkNames('/toto')).to.be.true
+//     }) 
+       
+// });
 
 
-describe("Validator checkNames()",()=>{
-    it("should return true ", ()=> {
-		expect(userValidator.checkNames('al ain')).to.be.true
-    })
-    it("should return true ", ()=> {
-		expect(userValidator.checkNames('toto')).to.be.true
-    })
-    it("should return true ", ()=> {
-		expect(userValidator.checkNames('to-to')).to.be.true
-    })
-    it("should return true ", ()=> {
-		expect(userValidator.checkNames('jules')).to.be.true
-    })
-    it("should return true ", ()=> {
-		expect(userValidator.checkNames('jules')).to.be.true
-    })
-    
-})
+describe("Validator checkPassword",()=>{
 
-describe("Validator checkNames()",()=>{
-  it("should return true ", ()=> {
-  expect(userValidator.checkNames('jules')).to.be.true
+  it("it should return false", ()=>{
+    expect(userValidator.checkPassword('1234@')).to.be.false
   })
-  it("should return true ", ()=> {
-  expect(userValidator.checkNames('jules')).to.be.true
+  it("it should return false", ()=>{
+    expect(userValidator.checkPassword('gk12@')).to.be.false
   })
-  it("should return true ", ()=> {
-  expect(userValidator.checkNames('jules')).to.be.true
+  it("it should return false", ()=>{
+    expect(userValidator.checkPassword('password')).to.be.false
   })
-  it("should return true ", ()=> {
-  expect(userValidator.checkNames('jules')).to.be.true
+  it("it should return false", ()=>{
+    expect(userValidator.checkPassword('123456789')).to.be.false
   })
-  it("should return true ", ()=> {
-  expect(userValidator.checkNames('jules')).to.be.true
+  it("it should return true", ()=>{
+    expect(userValidator.checkPassword('Abcd1234@')).to.be.true
+  })
+  it("it should return true", ()=>{
+    expect(userValidator.checkPassword('1234@VVVVnnj')).to.be.true
+  })
+  it("it should return false", ()=>{
+    expect(userValidator.checkPassword('abcd31/$')).to.be.false
   })
   
-})
-    
-	
 });
+
+
